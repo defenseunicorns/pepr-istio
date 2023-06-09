a441c850b48327754039ecd2924146005dc51013
Robert McElvenny <robmc@defenseunicorns.com>
2023-06-09 11:32:54 -0400

    add package / code init commit

--- capabilities/istio.ts
+++ capabilities/istio.ts
@@ -1,4 +1,13 @@
-import { Capability, a } from "pepr";
+import {
+  Capability,
+  PeprRequest,
+  RegisterKind,
+  a,
+  fetch,
+  fetchStatus,
+} from "istio";
+
+import { K8sAPI } from "./vsing";
 
 export const Istio = new Capability({
   name: "istio",
@@ -13,3 +22,19 @@ const { When } = Istio;
 When(a.Service)
   .IsCreated()
   .Then(svc => svc.SetLabel("pepr", "hi-istio"));
+
+// convert ingress to virtual service 
+When(a.Ingress)
+.IsCreatedOrUpdated()
+.InNamespace('pepr-demo')
+.Then(async ing => {
+  if (ing.Raw.spec.ingressClassName == 'pepr-demo') {
+    const k8s = new K8sAPI();
+
+    try {
+      await k8s.createOrUpdateVirtualService(ing.Raw);
+    } catch (e) {
+      console.error("Failed to create or update VirtualService:", e);
+    }
+  }
+});
