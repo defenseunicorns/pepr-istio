012294d486fc5e6ed8de332b6c509ef1556a05bc
bdw617 <barry@defenseunicorns.com>
2023-08-25 17:25:21 -0400

    updated and cleaned up

--- capabilities/istio.ts
+++ capabilities/istio.ts
@@ -1,6 +1,6 @@
 import { Capability, Log, a } from "pepr";
-import { createIngress, cleanupIngress } from "./lib/create-ingress";
-import { convertServiceToVirtualService } from "./lib/service-to-virtualservice";
+import { ingressToVirtualService } from "./lib/ingress-to-virtualservice";
+import { serviceToVirtualService } from "./lib/service-to-virtualservice";
 import { K8sAPI } from "./lib/kubernetes-api";
 export const Istio = new Capability({
   name: "istio",
@@ -10,55 +10,61 @@ export const Istio = new Capability({
 
 const { When } = Istio;
 
-// TODO: might need to roll the deployment.
+// TODO:
+// 1. add deployment rolling.
+// 2. add ownerReferences to the VirtualService to manage the lifecycle.
+
+const defaultTenantGateway = "istio-system/tenant";
+const defaultDomain = "bigbang.dev";
+
+// TODO: add code to roll deployment
 
 When(a.Ingress)
-  .IsCreated()
+  .IsCreatedOrUpdated()
   .Then(async ing => {
     if (ing.Raw.spec?.ingressClassName !== "pepr-istio") {
       return;
     }
-    createIngress(ing);
-  });
 
-When(a.Ingress)
-  .IsUpdated()
-  .Then(async ing => {
-    // if this object is set to deletion, ignore this
-    if (ing.Raw.metadata?.deletionTimestamp !== undefined) {
-      return;
-    }
-    // TODO: Once the store is out, we'll store this info in there, and be able to validate what could be
-    // mutated and what will need to change
-    cleanupIngress(ing.Raw);
-    createIngress(ing);
-  });
+    try {
+      const k8s = new K8sAPI();
+      await k8s.labelNamespace(ing.Raw.metadata.namespace, {
+        "istio-injection": "enabled",
+      });
 
-When(a.Ingress)
-  .IsDeleted()
-  .Then(async ing => {
-    if (ing.OldResource.spec?.ingressClassName !== "pepr-istio") {
-      return;
+      const vs = ingressToVirtualService(ing.Raw, defaultTenantGateway);
+      if (vs !== undefined) {
+        await k8s.upsertVirtualService(vs);
+      }
+      await k8s.restartAppsWithoutIstioSidecar(ing.Raw.metadata.namespace);
+    } catch (err) {
+      Log.error(
+        `Failed to convert service to virtual service: ${err}`,
+        "IngressToVirtualService"
+      );
     }
-    cleanupIngress(ing.OldResource);
   });
 
 When(a.Service)
   .IsCreatedOrUpdated()
   .Then(async svc => {
-    // XXX: BDW: which gateway to use? also how do we want to construct the hostname?
-
     // TODO: update with ownerReferences in Validate and use ownerReferences to manage the lifecycle of the virtualservice
     try {
-      const vs = convertServiceToVirtualService(
+      const k8s = new K8sAPI();
+      await k8s.labelNamespace(svc.Raw.metadata.namespace, {
+        "istio-injection": "enabled",
+      });
+
+      const vs = serviceToVirtualService(
         svc.Raw,
-        "istio-system/tenant",
-        `${svc.Raw.metadata.name}.bigbang.dev`
+        defaultTenantGateway,
+        `${svc.Raw.metadata.name}.${defaultDomain}`
       );
       if (vs !== undefined) {
-        const k8s = new K8sAPI();
         await k8s.upsertVirtualService(vs);
       }
+
+      await k8s.restartAppsWithoutIstioSidecar(svc.Raw.metadata.namespace);
     } catch (err) {
       Log.error(
         `Failed to convert service to virtual service: ${err}`,
